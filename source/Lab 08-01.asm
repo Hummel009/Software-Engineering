7b3698593681f19b0806cc8312881bc391a5d693
Hummel009 <emperorbur@gmail.com>
2022-04-27 21:09:57 +0300

    graph

---
+++ source/Lab 08-01.asm
@@ -0,0 +1,30 @@
+org 100h
+
+;====== LOAD MODE ======;
+        mov ax, 0013h
+        int 10h
+
+        push $A000
+        pop es
+
+        mov cx, 320 * 200
+        mov di, 0
+        mov si, 320
+drawLoop:
+        sub ax, 160 ; move screen up/down
+        sub dx, 160 ; move screen left/right
+
+        mov al, 60h ;fill screen with color
+
+        mov ax, di  ;smth random
+
+        xor ax, dx  ;smth random
+        imul ax, ax ;smth random
+        ;add here xor,and,or,mov,imul etc with ax, dx, di and si to get arts
+
+        stosb
+loop drawLoop
+
+        mov ah, 08h
+        int 21h
+ret
