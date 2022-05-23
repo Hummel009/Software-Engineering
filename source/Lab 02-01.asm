8e808dded1527219e72d0942ec026847e0bba907
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2022-05-23 20:46:47 +0300

    Update Lab 02-01.asm

--- source/Lab 02-01.asm
+++ source/Lab 02-01.asm
@@ -3,11 +3,11 @@ org 100h
 
 ;====== START ======;
         mov ah, 09h
-        mov dx, writeln
+        mov dx, str1
         int 21h
 
         mov ah, 0ah
-        mov dx, readln
+        mov dx, str2
         int 21h
 
         mov ah, 09h
@@ -15,22 +15,22 @@ org 100h
         int 21h
 
 ;====== SKIP IF 3 <> 5 ======;
-        mov al, [readln+4]
-        mov bl, [readln+6]
+        mov al, [str2+4]
+        mov bl, [str2+6]
 
         cmp al, bl
         jne skip
 
 ;====== SKIP IF N < 5 ======;
-        mov al, [readln+1]
+        mov al, [str2+1]
 
         cmp al, 5
         jl skip
 
 ;====== FIND AND TEST N-1 ======;
         mov bx, 0
-        mov bl, [readln+1]
-        mov al, [readln+bx]
+        mov bl, [str2+1]
+        mov al, [str2+bx]
 
         cmp al, 65
         jl skip
@@ -39,7 +39,7 @@ org 100h
         jg skip
 
 ;====== FIND AND TEST 3 ======;
-        mov al, [readln+4]
+        mov al, [str2+4]
 
         cmp al, 'A'
         jl skip
@@ -49,7 +49,7 @@ org 100h
 
 ;====== EVERYTHING IS OK ======;
         mov ah, 09h
-        mov dx, yes
+        mov dx, str3
         int 21h
 
         mov ah, 08h
@@ -60,7 +60,7 @@ ret
 ;====== EVERYTHING IS SHIT ======;
 skip:
         mov ah, 09h
-        mov dx, no
+        mov dx, str4
         int 21h
 
         mov ah, 08h
@@ -69,8 +69,8 @@ skip:
 ret
 
 ;====== VARIABLES ======;
-        writeln db "Enter the text: $"
-        readln db 7, 0, 7 dup ('$')
-        yes db 'Yes, this word is allowed.$'
-        no db 'No, this word is not allowed.$'
+        str1 db "Enter the text: $"
+        str2 db 7, 0, 7 dup ('$')
+        str3 db 'Yes, this word is allowed.$'
+        str4 db 'No, this word is not allowed.$'
         newLine db 13, 10, '$'    
