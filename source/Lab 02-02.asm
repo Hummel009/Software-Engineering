c2783038d63fcb45ff65037da82a50c9019d44cc
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2022-05-23 20:48:20 +0300

    Update Lab 02-02.asm

--- source/Lab 02-02.asm
+++ source/Lab 02-02.asm
@@ -3,11 +3,11 @@ org 100h
 
 ;====== START ======;
         mov ah, 09h
-        mov dx, writeln
+        mov dx, str1
         int 21h
 
         mov ah, 0ah
-        mov dx, readln
+        mov dx, str2
         int 21h
 
         mov ah, 09h
@@ -15,22 +15,22 @@ org 100h
         int 21h
 
 ;====== SKIP IF 1 <> 3 ======;
-        mov al, [readln+2]
-        mov bl, [readln+4]
+        mov al, [str2+2]
+        mov bl, [str2+4]
 
         cmp al, bl
         jne skip
 
 ;====== SKIP IF N < 3 ======;
-        mov al, [readln+1]
+        mov al, [str2+1]
 
         cmp al, 3
         jl skip
 
 ;====== FIND AND TEST N ======;
         mov bx, 0
-        mov bl, [readln+1]
-        mov al, [readln+bx+1]
+        mov bl, [str2+1]
+        mov al, [str2+bx+1]
 
         cmp al, 'A'
         jl skip
@@ -39,7 +39,7 @@ org 100h
         jg skip
 
 ;====== FIND AND TEST 5 ======;
-        mov al, [readln+6]
+        mov al, [str2+6]
 
         cmp al, 'A'
         jl skip
@@ -48,7 +48,7 @@ org 100h
         jg skip
 
 ;====== FIND AND TEST 1 ======;
-        mov al, [readln+2]
+        mov al, [str2+2]
 
         cmp al, '0'
         jl skip
@@ -58,7 +58,7 @@ org 100h
 
 ;====== EVERYTHING IS OK ======;
         mov ah, 09h
-        mov dx, yes
+        mov dx, str3
         int 21h
 
         mov ah, 08h
@@ -69,7 +69,7 @@ ret
 ;====== EVERYTHING IS SHIT ======;
 skip:
         mov ah, 09h
-        mov dx, no
+        mov dx, str4
         int 21h
 
         mov ah, 08h
@@ -78,8 +78,8 @@ skip:
 ret
 
 ;====== VARIABLES ======;
-        writeln db "Enter the text: $"
-        readln db 8, 0, 8 dup ('$')
-        yes db 'Yes, this word is allowed.$'
-        no db 'No, this word is not allowed.$'
-        newLine db 13, 10, '$'    
+        str1 db "Enter the text: $"
+        str2 db 8, 0, 8 dup ('$')
+        str3 db 'Yes, this word is allowed.$'
+        str4 db 'No, this word is not allowed.$'
+        newLine db 13, 10, '$'
