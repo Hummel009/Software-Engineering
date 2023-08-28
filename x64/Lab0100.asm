54804877f578aa160c3dd27ef8f18ed26e71b816
Jahor Burboŭski <60618853+Hummel009@users.noreply.github.com>
2023-08-28 17:49:40 +0300

    tabs

--- x64/Lab0100.asm
+++ x64/Lab0100.asm
@@ -6,68 +6,68 @@ include 'win64a.inc'
 section '.text' code readable executable
 
 Start:
-  invoke SetConsoleTitleA, conTitle
-  test eax, eax
-  jz Exit
+	invoke SetConsoleTitleA, conTitle
+	test eax, eax
+	jz Exit
 
-  invoke GetStdHandle, [STD_OUTP_HNDL]
-  mov [hStdOut], eax
+	invoke GetStdHandle, [STD_OUTP_HNDL]
+	mov [hStdOut], eax
 
-  invoke GetStdHandle, [STD_INP_HNDL]
-  mov [hStdIn], eax
+	invoke GetStdHandle, [STD_INP_HNDL]
+	mov [hStdIn], eax
 
-  invoke WriteConsoleA, [hStdOut], msg, msgLen, chrsWritten, 0   
-  invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
-  invoke ReadConsoleA, [hStdIn], readBuf, 255, chrsRead, 0
-  
-  mov al, [readBuf+8]
-  mov bl, [readBuf+9]
-  mov [readBuf+8], bl
-  mov [readBuf+9], al   
-  
-  mov al, [readBuf+2]
-  mov bl, [readBuf+5]
-  mov cl, [readBuf+8]
-  sub al, bl
-  add al, cl
-  mov [readBuf+4], al
-                          
-  sub [chrsRead], 2
+	invoke WriteConsoleA, [hStdOut], msg, msgLen, chrsWritten, 0	 
+	invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
+	invoke ReadConsoleA, [hStdIn], readBuf, 255, chrsRead, 0
+	
+	mov al, [readBuf+8]
+	mov bl, [readBuf+9]
+	mov [readBuf+8], bl
+	mov [readBuf+9], al	 
+	
+	mov al, [readBuf+2]
+	mov bl, [readBuf+5]
+	mov cl, [readBuf+8]
+	sub al, bl
+	add al, cl
+	mov [readBuf+4], al
 
-  invoke WriteConsoleA, [hStdOut], readBuf, [chrsRead], chrsWritten, 0    
+	sub [chrsRead], 2
 
-  invoke ReadConsoleA, [hStdIn], readBuf, 1, chrsRead, 0 
+	invoke WriteConsoleA, [hStdOut], readBuf, [chrsRead], chrsWritten, 0		
+
+	invoke ReadConsoleA, [hStdIn], readBuf, 1, chrsRead, 0 
 
 Exit:
-  invoke  ExitProcess, 0
+	invoke	ExitProcess, 0
 
 section '.data' data readable writeable
 
-  conTitle    db 'Hummel009', 0
-  msg         db 'Enter the text:', 0
-  msgLen      = $-msg
-  newLine     db 13, 10, 0  
-  newLineLen  = $-newLine
+	conTitle		db 'Hummel009', 0
+	msg				 db 'Enter the text:', 0
+	msgLen			= $-msg
+	newLine		 db 13, 10, 0	
+	newLineLen	= $-newLine
 
-  hStdIn      dd 0
-  hStdOut     dd 0
-  chrsRead    dd 0
-  chrsWritten dd 0
+	hStdIn			dd 0
+	hStdOut		 dd 0
+	chrsRead		dd 0
+	chrsWritten dd 0
 
-  STD_INP_HNDL  dd -10
-  STD_OUTP_HNDL dd -11
+	STD_INP_HNDL	dd -10
+	STD_OUTP_HNDL dd -11
 
 section '.bss' readable writeable
 
-  readBuf  db ?
+	readBuf	db ?
 
 section '.idata' import data readable
 
-  library kernel,'KERNEL32.DLL'
+	library kernel,'KERNEL32.DLL'
 
-  import kernel,\
-    SetConsoleTitleA, 'SetConsoleTitleA',\
-    GetStdHandle, 'GetStdHandle',\
-    WriteConsoleA, 'WriteConsoleA',\
-    ReadConsoleA, 'ReadConsoleA',\
-    ExitProcess, 'ExitProcess'
+	import kernel,\
+		SetConsoleTitleA, 'SetConsoleTitleA',\
+		GetStdHandle, 'GetStdHandle',\
+		WriteConsoleA, 'WriteConsoleA',\
+		ReadConsoleA, 'ReadConsoleA',\
+		ExitProcess, 'ExitProcess'
--- x64/Lab0200.asm
+++ x64/Lab0200.asm
@@ -6,124 +6,137 @@ include 'win64a.inc'
 section '.text' code readable executable
 
 Start:
-  invoke SetConsoleTitleA, conTitle
-  test eax, eax
-  jz Exit
+	invoke SetConsoleTitleA, conTitle
+	test eax, eax
+	jz Exit
 
-  invoke GetStdHandle, [STD_OUTP_HNDL]
-  mov [hStdOut], eax
+	invoke GetStdHandle, [STD_OUTP_HNDL]
+	mov [hStdOut], eax
 
-  invoke GetStdHandle, [STD_INP_HNDL]
-  mov [hStdIn], eax
+	invoke GetStdHandle, [STD_INP_HNDL]
+	mov [hStdIn], eax
 
-  invoke WriteConsoleA, [hStdOut], msg, msgLen, chrsWritten, 0   
-  invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
-  invoke ReadConsoleA, [hStdIn], readBuf, 255, chrsRead, 0
-  
-;====== SKIP IF 3 <> 5 ======;
-  ;invoke WriteConsoleA, [hStdOut], readBuf+2, 1, chrsWritten, 0   
-  ;invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
-  ;invoke WriteConsoleA, [hStdOut], readBuf+4, 1, chrsWritten, 0 
-  ;invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
+	invoke WriteConsoleA, [hStdOut], msg, msgLen, chrsWritten, 0	 
+	invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
+	invoke ReadConsoleA, [hStdIn], readBuf, 255, chrsRead, 0
+	
+;====== Skip IF 3 <> 5 ======;
+	mov al, [readBuf+2]
+	mov bl, [readBuf+4]
+	
+	cmp al, bl
+	jne Skip
 
-  mov al, [readBuf+2]
-  mov bl, [readBuf+4]
-  cmp al, bl
-  jne skip
-		     
-;====== FIND AND TEST 3 ======; 
-  mov al, [readBuf+2]
+;====== Skip IF N < 5 ======;
+	sub [chrsRead], 2
 
-  cmp al, 'A'
-  jl skip
+	cmp [chrsRead], 5
+	jl Skip
 
-  cmp al, 'z'
-  jg skip    
-                    
-;====== FIND LENGTH ======; 
-  mov ebx, 0
-  jmp iter
-
-sus:
-  inc ebx
-
-iter:
-  cmp [readBuf+ebx], 0
-  jne sus
-
-  sub ebx, 2
-  mov [readLen], bl
-  cmp [readLen], 10
-  jnl skip
-  
-  add [readLen], '0'
-
-  ;invoke WriteConsoleA, [hStdOut], readLen, 1, chrsWritten, 0   
-  ;invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
-  
-;====== SKIP IF N < 5 ======;
-  cmp [readLen], 5
-  jl skip
-  
 ;====== FIND AND TEST N-1 ======;
-  movzx ebx, [readLen]
-  mov al, [readBuf+ebx]
+	mov bx, 0
+	mov bl, [str2+1]
+	mov al, [str2+bx]
 
-  cmp al, 'A'
-  jl skip
+	cmp al, 'A'
+	jl Skip
 
-  cmp al, 'z'
-  jg skip
-  
+	cmp al, 'z'
+	jg Skip
+
+;====== FIND AND TEST 3 ======; 
+	mov al, [readBuf+2]
+
+	cmp al, 'A'
+	jl Skip
+
+	cmp al, 'z'
+	jg Skip		
+
+;====== FIND LENGTH ======; 
+	mov ebx, 0
+	jmp Iter
+
+Sus:
+	inc ebx
+
+Iter:
+	cmp [readBuf+ebx], 0
+	jne Sus
+
+	sub ebx, 2
+	mov [readLen], bl
+	cmp [readLen], 10
+	jnl Skip
+	
+	add [readLen], '0'
+
+	;invoke WriteConsoleA, [hStdOut], readLen, 1, chrsWritten, 0	 
+	;invoke WriteConsoleA, [hStdOut], newLine, newLineLen, chrsWritten, 0
+	
+;====== Skip IF N < 5 ======;
+	cmp [readLen], 5
+	jl Skip
+	
+;====== FIND AND TEST N-1 ======;
+	movzx ebx, [readLen]
+	mov al, [readBuf+ebx]
+
+	cmp al, 'A'
+	jl Skip
+
+	cmp al, 'z'
+	jg Skip
+	
 ;====== EVERYTHING IS OK ======;
-        
+				
 	invoke WriteConsoleA, [hStdOut], allowed, allowedLen, chrsWritten, 0
 
-	jmp finish
+	jmp Finish
 		
 ;====== EVERYTHING IS NOT OK ======;
-  skip:
+Skip:
 	invoke WriteConsoleA, [hStdOut], disallowed, disallowedLen, chrsWritten, 0
 
 ;====== DO NOT CLOSE ======;
-  finish:		
-  invoke ReadConsoleA, [hStdIn], readBuf, 1, chrsRead, 0
+Finish:		
+	invoke ReadConsoleA, [hStdIn], readBuf, 1, chrsRead, 0
 
 Exit:
-  invoke  ExitProcess, 0
+	invoke	ExitProcess, 0
 
 section '.data' data readable writeable
 
-  conTitle          db 'Hummel009', 0
-  msg               db 'Enter the text:', 0
-  msgLen            = $-msg
-  newLine           db 13, 10, 0  
-  newLineLen        = $-newLine
-  allowed           db 'Yes, this word is allowed.'
-  allowedLen        = $-allowed
-  disallowed        db 'No, this word is not allowed.'
-  disallowedLen     = $-disallowed
+	conTitle					db 'Hummel009', 0
+	msg							 db 'Enter the text:', 0
+	msgLen						= $-msg
+	newLine					 db 13, 10, 0	
+	newLineLen				= $-newLine
+	allowed					 db 'Yes, this word is allowed.'
+	allowedLen				= $-allowed
+	disallowed				db 'No, this word is not allowed.'
+	disallowedLen		 = $-disallowed
 
-  hStdIn      dd 0
-  hStdOut     dd 0
-  chrsRead    dd 0
-  chrsWritten dd 0
+	hStdIn			dd 0
+	hStdOut		 dd 0
+	chrsRead		dd 0
+	chrsWritten dd 0
 
-  STD_INP_HNDL  dd -10
-  STD_OUTP_HNDL dd -11
+	STD_INP_HNDL	dd -10
+	STD_OUTP_HNDL dd -11
 
 section '.bss' readable writeable
 
-  readBuf  db ? 
-  readLen  db ?     
+	readBuf	db ? 
+	readLen	db ?		 
 
 section '.idata' import data readable
 
-  library kernel,'KERNEL32.DLL'
+	library kernel,'KERNEL32.DLL'
 
-  import kernel,\
-    SetConsoleTitleA, 'SetConsoleTitleA',\
-    GetStdHandle, 'GetStdHandle',\
-    WriteConsoleA, 'WriteConsoleA',\
-    ReadConsoleA, 'ReadConsoleA',\
-    ExitProcess, 'ExitProcess'
+	import kernel,\
+		SetConsoleTitleA, 'SetConsoleTitleA',\
+		GetStdHandle, 'GetStdHandle',\
+		WriteConsoleA, 'WriteConsoleA',\
+		ReadConsoleA, 'ReadConsoleA',\
+		ExitProcess, 'ExitProcess'
